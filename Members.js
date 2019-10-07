c4552302b153efaa7f20937b5026cc8c316e581c
Kris Lafrance <Kris.lafrance6067@gmail.com>
2019-10-07 15:08:56 -0700

    adding API routing

--- Members.js
+++ Members.js
@@ -1,13 +1,16 @@
 const members = [
 	{
+		id: 1,
 		name: "Bruce Wayne",
 		occupation: "Crime Fighter"
 	},
 	{
+		id: 2,
 		name: "Dick Grayson",
 		occupation: "Cab Driver"
 	},
 	{
+		id: 3,
 		name: "Jason Todd",
 		occupation: "Teacher"
 	}
--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 const express = require("express");
 const path = require("path");
 const logger = require("./middleware/logger");
-const members = require("./Members");
+
 const app = express();
 
 // Init middleware
-app.use(logger);
-
-// Gets all members
-app.get("/api/members", (req, res) => {
-	res.json(members);
-});
+//app.use(logger);
 
 // Set static folderee
 app.use(express.static(path.join(__dirname, "public")));
+
+// Members API Routes
+app.use("/api/members", require("./routes/api/members"));
 
 // check the environment variable port first then default to 5000
 const PORT = process.env.PORT || 5000;
