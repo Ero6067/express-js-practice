166ab9044d5da1dc16ec62e7161d62f29e7d2b23
Kris Lafrance <Kris.lafrance6067@gmail.com>
2019-10-07 15:29:25 -0700

    adding create member functionality

--- routes/api/members.js
+++ routes/api/members.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const uuid = require("uuid");
 const members = require("../../Members");
 
 // Gets all members
@@ -16,6 +17,38 @@ router.get("/:id", (req, res) => {
 	} else {
 		res.status(400).json({ msg: `No member with the id of ${req.params.id}` });
 	}
+});
+
+// Create Member
+router.post("/", (req, res) => {
+	const newMember = {
+		id: uuid.v4(),
+		name: req.body.name,
+		profession: req.body.profession
+	};
+
+	if (!newMember.name) {
+		return res.status(400).json({ msg: "Please include a name" });
+	}
+
+	members.push(newMember);
+	res.json(members);
+});
+
+// Update Member
+router.post("/", (req, res) => {
+	const newMember = {
+		id: uuid.v4(),
+		name: req.body.name,
+		profession: req.body.profession
+	};
+
+	if (!newMember.name) {
+		return res.status(400).json({ msg: "Please include a name" });
+	}
+
+	members.push(newMember);
+	res.json(members);
 });
 
 module.exports = router;
