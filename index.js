f39c12c5136ba17df48ac22e7087f028c9eabd66
Kris Lafrance <Kris.lafrance6067@gmail.com>
2019-10-07 23:25:59 -0700

    adding express-handlebars to project

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 const express = require("express");
 const path = require("path");
-const logger = require("./middleware/logger");
+const exphbs = require("express-handlebars");
 
 const app = express();
 
-// Init middleware
-//app.use(logger);
+// Handlebars Middleware
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 //Body Parser Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// Set static folderee
+// Homepage Route
+app.get("/", (req, res) => res.render("index"));
+
+// Set static folder
 app.use(express.static(path.join(__dirname, "public")));
 
 // Members API Routes
